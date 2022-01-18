174a4286fe67d13100a976a7c643dbac09b57716
Eduhartg <66975371+Eduhartg@users.noreply.github.com>
2022-01-18 12:47:35 -0300

    Agregar archivos de proyecto.

---
+++ Xamarin.WeightConverter/Xamarin.WeightConverter/Converter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xamarin.WeightConverter
+{
+    public class Converter
+    {
+        private static double result = 0;
+        private static readonly int round = 2;
+        public static string ToPounds(string kilos)
+        {
+            if (!Double.TryParse(kilos, out result))
+                return "0";
+
+            if (result == 0) return "0";
+
+            return Math.Round(result * 2.2046, round).ToString();
+        }
+        public static string ToKilos(string pounds)
+        {
+            if (!Double.TryParse(pounds, out result))
+                return "0";
+
+            if (result == 0) return "0";
+
+            return Math.Round(result * 0.453592, round).ToString();
+        }
+    }
+}
---
+++ Xamarin.WeightConverter/Xamarin.WeightConverter/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Xamarin.WeightConverter
+{
+    public partial class MainPage : ContentPage
+    {
+        private bool converting;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Entry_KilosChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!converting)
+            {
+                converting = true;
+                poundsText.Text = Converter.ToPounds(kilosText.Text);
+            }
+            else converting = false;
+        }
+        private void Entry_PoundsChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!converting)
+            {
+                converting = true;
+                kilosText.Text = Converter.ToKilos(poundsText.Text);
+            }
+            else converting = false;
+        }
+
+        private void weightText_Focused(object sender, FocusEventArgs e)
+        {
+            if (((Entry)sender).Text == "0")
+            {
+                ((Entry)sender).Text = "";
+                converting = false;
+            }
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            System.Environment.Exit(0);
+            //Process.GetCurrentProcess().CloseMainWindow();
+        }
+    }
+}
